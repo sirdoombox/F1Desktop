e7b0ee87c694026d2c377aa483d0784f0980ab22
Aaron <aaron.j.king.95@gmail.com>
2022-07-28 00:21:42 +0100

    Got a window showing

---
+++ Features/Calendar/CalendarRootViewModel.cs
@@ -0,0 +1,14 @@
+﻿using F1Desktop.Services;
+using Stylet;
+
+namespace F1Desktop.Features.Calendar;
+
+public class CalendarRootViewModel : Screen
+{
+    private ErgastAPIService _api;
+    
+    public CalendarRootViewModel(ErgastAPIService api)
+    {
+        _api = api;
+    }
+}
--- Features/Root/RootViewModel.cs
+++ Features/Root/RootViewModel.cs
@@ -1,4 +1,5 @@
-﻿using F1Desktop.Services;
+﻿using F1Desktop.Features.Calendar;
+using F1Desktop.Services;
 using JetBrains.Annotations;
 using Stylet;
 
@@ -7,5 +8,18 @@ namespace F1Desktop.Features.Root;
 [UsedImplicitly]
 public class RootViewModel : Conductor<IScreen>
 {
+    public CalendarRootViewModel Calendar { get; }
+
+    private readonly IWindowManager _wm;
     
+    public RootViewModel(IWindowManager wm, CalendarRootViewModel calendar)
+    {
+        Calendar = calendar;
+        _wm = wm;
+    }
+
+    public void OpenWindow(Screen toOpen)
+    {
+        _wm.ShowWindow(toOpen);
+    }
 }
