f208aad0a68f8aefdd84bef89ef534371567845b
Aaron <aaron.j.king.95@gmail.com>
2022-08-07 09:00:39 +0100

    Implement content switcher control.

---
+++ Controls/ContentSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace F1Desktop.Controls;
+
+public class ContentSwitcher : Control
+{
+    public static readonly DependencyProperty UseAlternateContentProperty = DependencyProperty.Register(
+        nameof(UseAlternateContent), typeof(bool), typeof(ContentSwitcher), new PropertyMetadata(default(bool)));
+
+    public bool UseAlternateContent
+    {
+        get => (bool)GetValue(UseAlternateContentProperty);
+        set => SetValue(UseAlternateContentProperty, value);
+    }
+
+    public static readonly DependencyProperty DefaultContentProperty = DependencyProperty.Register(
+        nameof(DefaultContent), typeof(object), typeof(ContentSwitcher), new PropertyMetadata(default(object)));
+
+    public object DefaultContent
+    {
+        get => (object)GetValue(DefaultContentProperty);
+        set => SetValue(DefaultContentProperty, value);
+    }
+
+    public static readonly DependencyProperty AlternateContentProperty = DependencyProperty.Register(
+        nameof(AlternateContent), typeof(object), typeof(ContentSwitcher), new PropertyMetadata(default(object)));
+
+    public object AlternateContent
+    {
+        get => (object)GetValue(AlternateContentProperty);
+        set => SetValue(AlternateContentProperty, value);
+    }
+}
---
+++ Converters/NullToBoolConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace F1Desktop.Converters;
+
+public class NullToBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is null;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => 
+        throw new NotImplementedException();
+}
